4a73ab4c8a851f69d493850b3bc0db71569e6f67
ronni <ronni@RON-ASUS-ROG>
2024-07-05 12:57:43 -0400

    ..

--- frontend2/src/app/app.component.ts
+++ frontend2/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import createApp, { AppBridgeState, ClientApplication } from '@shopify/app-bridge';
+import { ResourcePicker } from "@shopify/app-bridge/actions";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,36 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public app: ClientApplication<AppBridgeState> | undefined;
+  // @ts-ignore
+  public picker: ResourcePicker;
+
+  ngOnInit(): void {
+    let config: any = {
+      apiKey: "5c69d4664f81a66c6d76cb39bd353cf6",
+      // The host of the specific shop that's embedding your app. This value is provided by Shopify as a URL query parameter that's appended to your application URL when your app is loaded inside the Shopify admin.
+      host: new URLSearchParams(location.search).get("host"),
+      forceRedirect: true
+    };
+    this.app = createApp(config);
+
+    this.picker = ResourcePicker.create(this.app, {
+      resourceType: ResourcePicker.ResourceType.Product
+    });
+    this.picker.subscribe(ResourcePicker.Action.SELECT, (payload:any) => {
+      console.log(payload.selection);
+    });
+
+  }
+
+  public openPicker(): void {
+    this.picker.dispatch(ResourcePicker.Action.OPEN);
+
+  }
+
   title = 'frontend2';
+
 }
+
+
