c23a839ef625c7e6551f00a926d0558d1510850a
ronni <ronni@RON-ASUS-ROG>
2024-07-14 17:48:30 -0400

    ..

--- frontend2/src/app/app.component.ts
+++ frontend2/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import createApp, { AppBridgeState, ClientApplication } from '@shopify/app-bridge';
-import { ResourcePicker } from "@shopify/app-bridge/actions";
+import { ResourcePicker, Toast } from "@shopify/app-bridge/actions";
 import { getSessionToken } from "@shopify/app-bridge/utilities"
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Action } from "@shopify/app-bridge-core/actions/Toast";
 
 @Component({
   selector: 'app-root',
@@ -19,14 +20,16 @@ export class AppComponent implements OnInit {
   public token: string | undefined;
   title = 'frontend2';
 
-
   ngOnInit(): void {
+
+
     let config: any = {
       apiKey: "5c69d4664f81a66c6d76cb39bd353cf6",
       // The host of the specific shop that's embedding your app. This value is provided by Shopify as a URL query parameter that's appended to your application URL when your app is loaded inside the Shopify admin.
       host: new URLSearchParams(location.search).get("host"),
       forceRedirect: true
     };
+
     this.app = createApp(config);
     getSessionToken(this.app).then(x => {
       this.token = x;
@@ -45,9 +48,9 @@ export class AppComponent implements OnInit {
   }
 
   public callController(): void {
-    const header = new HttpHeaders().set('Authorization', 'Bearer '+this.token+'' ); // may be localStorage/sessionStorage
-    const headers = { headers: header };
-    this.client.get("/api/products",headers).subscribe({
+    const header = new HttpHeaders().set('Authorization', 'Bearer ' + this.token + ''); // may be localStorage/sessionStorage
+    const headers = {headers: header};
+    this.client.get("/api/products", headers).subscribe({
       next: (x) => {
         alert("success");
       },
@@ -63,6 +66,18 @@ export class AppComponent implements OnInit {
   }
 
 
+
+  showToad() {
+    alert('');
+    // @ts-ignore
+   var t =  Toast.create(this.app,{
+      action: 'ok',
+      isError: true,
+      message: 'Something went wrong',
+
+    });
+   t.dispatch(Action.SHOW);
+  }
 }
 
 
