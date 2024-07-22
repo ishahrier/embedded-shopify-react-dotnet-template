808d91eec59385c7eac475c6cc4a5f8af849594d
Md Ishtiaque Shahrier <ishahrier@gmail.com>
2024-07-22 00:16:12 -0400

    ..

---
+++ frontend2/src/app/pages/toast-test/toast-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toast-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './toast-test.component.html',
+  styleUrl: './toast-test.component.css'
+})
+export class ToastTestComponent {
+
+}
